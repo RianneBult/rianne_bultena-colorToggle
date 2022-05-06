d6d1bb3e2b420b1eb622d066a54b3c7ca91ed0d7
RianneBult <84838025+RianneBult@users.noreply.github.com>
2022-05-06 11:10:49 +0200

    Update script.js
    1. addEventListeners with 'mouse' replaced to one-line
    2. Added function 'addTextToBody' so we can reuse it
    3. Added 'classList forEach' in toggleBackgroundColor-function so we can remove the background color classes & remove h1 text
    4. Replaced if/else to switch statement
    5. Added radioButtons.forEach at the bottom to toggle background color
    6. This provides less text and more overview

--- script.js
+++ script.js
@@ -3,40 +3,48 @@ const list = document.querySelector("ul");
 const radioButtons = document.querySelectorAll("input");
 const menu = document.querySelector("nav");
 
-menu.addEventListener("mousemove", () => {
-    list.classList.add("toggle-list");
-});
+menu.addEventListener("mousemove", () => listMenu.classList.add("toggle-list"));
+menu.addEventListener("mouseout", () => listMenu.classList.remove("toggle-list"));
 
-menu.addEventListener("mouseout", () => {
-    list.classList.remove("toggle-list");
-});
+const addTextToBody = (text) => {
+    const textColor = document.createElement("h1");
+    textColor.textContent = text;
+    body.append(textColor);
+}
 
-const textColor = document.createElement("h1");
-body.append(textColor);
-
-radioButtons.forEach(input => {
-    input.addEventListener("click", e => {
-        list.classList.remove("toggle-list");
-        if (e.target.matches("input[value=grey]")) {
-            body.classList.add("list-color-grey");
-            body.classList.remove("list-color-orange", "list-color-blue", "list-color-red", "list-color-green");
-            textColor.textContent = "Grijs";
-        } else if (e.target.matches("input[value=orange]")) {
-            body.classList.add("list-color-orange");
-            body.classList.remove("list-color-grey", "list-color-blue", "list-color-red", "list-color-green");
-            textColor.textContent = "Oranje";
-        } else if (e.target.matches("input[value=blue]")) {
-            body.classList.add("list-color-blue");
-            body.classList.remove("list-color-grey", "list-color-orange", "list-color-red", "list-color-green");
-            textColor.textContent = "Blauw";
-        } else if (e.target.matches("input[value=red]")) {
-            body.classList.add("list-color-red");
-            body.classList.remove("list-color-grey", "list-color-orange", "list-color-blue", "list-color-green");
-            textColor.textContent = "Rood";
-        } else if (e.target.matches("input[value=green]")) {
-            body.classList.add("list-color-green");
-            body.classList.remove("list-color-grey", "list-color-orange", "list-color-blue", "list-color-red");
-            textColor.textContent = "Groen";
+const toggleBackgroundColor = (e) => {
+    body.classList.forEach(item => {
+        if (item.startsWith("list")) {
+            body.classList.remove(item);
         };
     });
-});
+    body.querySelectorAll("h1").forEach(e => e.remove());
+
+    switch (e.target.value) {
+        case "grey":
+            body.classList.add("list-color-grey");
+            addTextToBody("Grijs");
+            break;
+        case "orange":
+            body.classList.add("list-color-orange");
+            addTextToBody("Oranje");
+            break;
+        case "blue":
+            body.classList.add("list-color-blue");
+            addTextToBody("Blauw");
+            break;
+        case "red":
+            body.classList.add("list-color-red");
+            addTextToBody("Rood");
+            break;
+        case "green":
+            body.classList.add("list-color-green");
+            addTextToBody("Groen");
+            break;
+        default:
+            body.classList.add("list-color-grey");
+            break;
+    }
+}
+
+radioButtons.forEach(input => input.addEventListener("click", toggleBackgroundColor));
